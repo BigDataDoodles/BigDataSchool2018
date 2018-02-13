b9d100892c905bcee3054d24d624dedb78e94ba0
Mikhail Mikhaylov <MikhYuMikhaylov@spb.beeline.ru>
2018-02-13 11:54:53 +0300

    commit task 1

---
+++ bigdata-school-task1/src/test/java/ru/vsu/bigdata/task1/PokemonTest.java
@@ -0,0 +1,16 @@
+package ru.vsu.bigdata.task1;
+
+import org.junit.Test;
+
+//TODO Необходимо создать еще один вспомогательный класс TestUtils
+//TODO В классе TestUtils реализовать статические методы: чтение/запись данных из/в файл
+//TODO Метод чтения файла, должен возвращать List<Pair<KEY_IN, VALUE_IN>>. Далее эта коллекция передается в качестве аргумента в метод addAll() в экземпляр класса MapReduceDriver
+//TODO Метод записи файла, в качестве параметра принимает коллекцию возвращаемую методом run() экземпляра класса MapReduceDriver
+public class PokemonTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
