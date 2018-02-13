7d3568a23d6e76c8d4931d9f6d65ea2d84ea4533
Mikhail Mikhaylov <MikhYuMikhaylov@spb.beeline.ru>
2018-02-13 10:44:02 +0300

    wordcount

--- src/main/java/ru/vsu/bigdata_course/staging/stage1/MStage1.java
+++ src/main/java/ru/vsu/bigdata_course/staging/stage1/MStage1.java
@@ -1,4 +1,19 @@
 package ru.vsu.bigdata_course.staging.stage1;
 
-public class MStage1 {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class MStage1 extends Mapper<NullWritable, Text, Text, IntWritable> {
+
+    @Override
+    protected void map(NullWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String[] row = value.toString().split(" ", -1);
+        for (String v : row)
+            context.write(new Text(v), new IntWritable(1));
+    }
+
 }
--- src/test/java/ru/vsu/bigdata_course/Stage1Test.java
+++ src/test/java/ru/vsu/bigdata_course/Stage1Test.java
@@ -1,11 +1,27 @@
 package ru.vsu.bigdata_course;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.types.Pair;
 import org.junit.Test;
+import ru.vsu.bigdata_course.staging.stage1.MStage1;
+import ru.vsu.bigdata_course.staging.stage1.RStage1;
+
+import java.io.IOException;
+import java.util.List;
 
 public class Stage1Test {
 
     @Test
-    public void test() {
+    public void test() throws IOException {
+        MapReduceDriver<NullWritable, Text, Text, IntWritable, Text, IntWritable> driver =
+                MapReduceDriver.newMapReduceDriver(new MStage1(), new RStage1());
+        driver.addInput(NullWritable.get(), new Text("a a b b c a e"));
+        List<Pair<Text, IntWritable>> res = driver.run();
+        for (Pair<Text, IntWritable> pair : res)
+            System.out.println(pair.getFirst() + "\t" + pair.getSecond());
     }
 
 }
