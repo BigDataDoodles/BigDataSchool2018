428c4f8004486dc95b8edc51f20af0bccf8fa658
Tenfleques <tendaichikake@gmail.com>
2018-03-01 19:49:05 +0300

    start the scala project

--- src/main/java/ru/vsu/bigdata_course/staging/stage1/RStage1.java
+++
@@ -1,4 +0,0 @@
-package ru.vsu.bigdata_course.staging.stage1;
-
-public class RStage1 {
-}
---
+++ src/main/scala/ru/bigdata/course/system/Application.scala
@@ -0,0 +1,8 @@
+package ru.bigdata.course.system
+
+/**
+  * Created by gennady on 01/03/18.
+  */
+object Application extends App{
+  println("Начнем отсюда")
+}
